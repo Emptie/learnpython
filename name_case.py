558e99ae9e5d698eaf54aba019bcc5194d899b19
Emptie <emptielol@gmail.com>
2018-02-03 13:48:29 +0800

    name cases practice

---
+++ name_case.py
@@ -0,0 +1,7 @@
+name = "emptie"
+print("Hello " + name.title() +", would you like to learn some Python today?" )
+print("Hello " + name.upper() +", would you like to learn some Python today?" )
+print("Hello " + name.lower() +", would you like to learn some Python today?" )
+
+famous_name = "albert einstein"
+print(famous_name.title() + " once said, \"A person who never made a mistake never tried anything new.\"")
