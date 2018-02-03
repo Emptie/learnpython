c3475d9152c784967a171431fa98cca9f1943e43
Emptie <emptielol@gmail.com>
2018-02-03 13:21:59 +0800

    name.py

---
+++ name.py
@@ -0,0 +1,11 @@
+first_name = "ada"
+last_name = "lovelace"
+full_name = first_name + " " + last_name
+print("Hello, " + full_name.title() + "!")
+
+print("Languages:\n\tPython\n\tC\n\tJavaScript")
+
+favorite_language = " Python "
+print(favorite_language.rstrip())
+print(favorite_language.lstrip())
+print(favorite_language.strip())
