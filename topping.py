45cabf1d43bfb61e5e6b82780dd26300a87985e8
Emptie <emptielol@gmail.com>
2018-02-12 00:10:36 +0800

    add topping.py

---
+++ topping.py
@@ -0,0 +1,8 @@
+requested_topping = ['mushrooms','extra cheese']
+if 'mushrooms' in requested_topping:
+    print("Adding mushrooms.")
+elif 'pepperoni' in requested_topping:
+    print("Adding pepperoni.")
+elif 'extra cheese' in requested_topping:
+    print("Adding extra cheese.")
+print("\nFinished making your pizza!")
