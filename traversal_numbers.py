ff0ff640a976e78d16d7d7908a24c41904a286ce
Emptie <emptielol@gmail.com>
2018-02-12 13:06:54 +0800

    add traversal_numbers.py

---
+++ traversal_numbers.py
@@ -0,0 +1,11 @@
+L = [i for i in range(1,10)]
+
+for num in L:
+    if num == 1:
+        print("1st")
+    elif num == 2:
+        print("2nd")
+    elif num == 3:
+        print("3rd")
+    else:
+        print(str(num) + "th")
