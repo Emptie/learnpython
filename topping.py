33d230b4917407464e3113deaf462ac7a59ec35b
Emptie <emptielol@gmail.com>
2018-02-12 12:57:47 +0800

    more lists

--- topping.py
+++ topping.py
@@ -1,8 +1,9 @@
-requested_toppings = ['mushrooms','green peppers','extra cheese']
+available_toppings = ['mushrooms','olives','green peppers','pepperoni','pineapple','extra cheese']
+requested_toppings = ['mushrooms','french fries','extra cheese']
 
-if requested_toppings:
-    for requested_topping in requested_toppings:
-        print("Adding " + requested_topping + ".")
-    print("\nFinished making your pizza!")
-else:
-    print("Are you sue you want a plain pizza?")
+for requested_topping in requested_toppings:
+    if requested_topping in available_toppings:
+        print("Add " + requested_topping + ".")
+    else:
+        print("Sorry, we don't have " + requested_topping + ".")
+print("\nFinished making your pizza!")
