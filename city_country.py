51078a5b1cf9cb257e229ad4cbdde6be5ef1e46c
Emptie <emptielol@gmail.com>
2018-02-13 15:46:57 +0800

    定义函数练习

---
+++ city_country.py
@@ -0,0 +1,6 @@
+def city_country(city,country="china"):
+    print("\"" + city.title() + ", " + country.title()+ "\"")
+
+city_country('santiago','chile')
+city_country('shanghai')
+city_country(city='beijing')
